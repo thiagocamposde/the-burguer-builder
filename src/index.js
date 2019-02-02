fb0adcdbba9f36417ad34e441b292d0f0ce5656c
Thiago de Campos <thiagocamposde@gmail.com>
2019-02-02 20:56:26 -0200

    First setup with component example

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import FormContainer from "./js/components/containers/FormContainer.jsx";
+
+reactDOM.render(<FormContainer />, document.getElementById('index'));
